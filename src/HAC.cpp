cedf58c7d6906e937be37728555f9a37bba8be6a
Mike M <mikemaddem@gmail.com>
2020-04-13 22:02:41 -0400

    update cmake file

--- src/HAC.cpp
+++ src/HAC.cpp
@@ -2,3 +2,22 @@
 // Created by mikem on 4/13/2020.
 //
 
+#include <iostream>
+#include <windows.h>
+#include <stdio.h>
+#include <tchar.h>
+#include <psapi.h>
+#include <windows.h> //include windows.h header file (needed for windows API)
+#include <tlhelp32.h> //include tlhelp32.h
+#include <tchar.h>	//tchar.h header file
+#include <iostream> //include iostream header file for input/output operations
+#include <string> //include string header (needed for some string operations)
+
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    DWORD aProcess[1024], cbNeeded, cProcesses;
+    HANDLE hProcess = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+    std::cout << hProcess;
+    return 0;
+}
